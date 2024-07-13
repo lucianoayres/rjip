7fecd32a0e208823e947d4052ccfcddeb1ea18e8
Luciano Ayres <lucianofarias@gmail.com>
2024-07-13 19:37:42 -0300

    test: add update_operations unit test

--- rji_cli/update_operations.py
+++ rji_cli/update_operations.py
@@ -1,3 +1,10 @@
+import json 
+
 def update_last_pick_json(last_pick_json_file_path, new_item):
     """Update last pick JSON file."""
-    return False
+    with open(last_pick_json_file_path, 'r+') as file:
+        data = json.load(file)
+        data.append(new_item)
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
---
+++ tests/test_update_operations.py
@@ -0,0 +1,40 @@
+import unittest
+import os
+import json
+from rji_cli.update_operations import update_last_pick_json
+
+class TestUpdateLastPickJson(unittest.TestCase):
+
+    def setUp(self):
+        self.json_data = [
+            {"id": 1, "name": "Item 1"},
+            {"id": 2, "name": "Item 2"},
+            {"id": 3, "name": "Item 3"},
+        ]
+        # Create a temporary file with initial JSON data
+        self.temp_file = 'temp_file.json'
+        with open(self.temp_file, 'w') as f:
+            json.dump(self.json_data, f)
+
+    def tearDown(self):
+        # Clean up the temporary file after each test
+        if os.path.exists(self.temp_file):
+            os.remove(self.temp_file)
+    
+    def test_update_last_pick_json(self):
+        new_item = {"id": 4, "name": "Item 4" }
+        update_last_pick_json(self.temp_file, new_item)
+
+        with open(self.temp_file, 'r') as f:
+            updated_data = json.load(f)
+
+        expected_data = [
+            {"id": 1, "name": "Item 1"},
+            {"id": 2, "name": "Item 2"},
+            {"id": 3, "name": "Item 3"},
+            {"id": 4, "name": "Item 4"}
+        ]
+        self.assertEqual(updated_data, expected_data)
+    
+if __name__ == "__main__":
+    unittest.main()
